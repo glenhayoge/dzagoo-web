9047edaa7ddd0c033850629526c0acecd241ebe1
glenhayoge <glenhayoge@gmail.com>
2022-10-24 12:34:38 +1000

    fix: favicons, message menu mobile

--- src/contact.php
+++ src/contact.php
@@ -19,7 +19,7 @@
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
+        // Update this to your desired email address.
         $recipient = "";
 
         // Set the email subject.
